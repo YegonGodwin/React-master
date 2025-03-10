d51e7d28d2dccb31c6cd88013af09578bd87f2a0
Godwin Kibet <kibetgodwin13@gmail.com>
2025-03-10 20:56:11 +0300

    #Bug fixed

--- src/components/Names.tsx
+++ src/components/Names.tsx
@@ -1,20 +1,31 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function Names() {
   const fruits = [
     { id: 1, title: "Banana", color: "Yellow" },
     { id: 2, title: "Orange", color: "Yellow" },
     { id: 3, title: "Apple", color: "red" },
   ];
-  const Fruit = ({ fruit }) => (
+  const Fruit = ({
+    fruit,
+  }: {
+    fruit: { id: number; title: string; color: string };
+  }) => (
     <li>
-      {fruit.fruit} is {fruit.color}
+      {fruit.title} is {fruit.color}
     </li>
   );
   return (
-    <ul>
-      {fruits.map((fruit) => (
-        <Fruit key={fruit.id} fruit={fruit} />
-      ))}
-    </ul>
+    <>
+      <h3 className="text-primary fst-italic">
+        This is mapping function in React
+      </h3>
+      <ul>
+        {fruits.map((fruit) => (
+          <Fruit key={fruit.id} fruit={fruit} />
+        ))}
+      </ul>
+    </>
   );
 }
 
