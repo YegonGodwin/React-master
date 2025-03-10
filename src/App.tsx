70f241f683f8d93d51000d6ddea4bd8aca1f1baa
Godwin Kibet <kibetgodwin13@gmail.com>
2025-03-10 20:01:02 +0300

    #Bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,10 @@
 import BookCard from "./components/BookCard";
-import "./Style.css";
 
 function App() {
-  const books = [
-    {
-      id: 1,
-      title: "To Kill a Mockingbird",
-      author: "Harper Lee",
-      rating: 4.8,
-      image:
-        "https://images.unsplash.com/photo-1601828519833-892648ce83f1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bW9ja2luZ2JpcmR8ZW58MHx8MHx8fDA%3D",
-    },
-    {
-      id: 2,
-      title: "1984",
-      author: "George Orwell",
-      rating: 4.6,
-      image:
-        "https://images.unsplash.com/photo-1601828519833-892648ce83f1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bW9ja2luZ2JpcmR8ZW58MHx8MHx8fDA%3D",
-    },
-    {
-      id: 3,
-      title: "The Great Gatsby",
-      author: "F. Scott Fitzgerald",
-      rating: 4.3,
-      image: "https://source.unsplash.com/200x300/?book,jazz",
-    },
-    {
-      id: 4,
-      title: "Pride and Prejudice",
-      author: "Jane Austen",
-      rating: 4.7,
-      image: "https://source.unsplash.com/200x300/?book,romance",
-    },
-  ];
-
   return (
-    <div className="container my-5">
-      <h1 className="text-center mb-4">Book List</h1>
-      <div className="row">
-        {books.map((book) => (
-          <BookCard key={book.id} book={book} />
-        ))}
-      </div>
-    </div>
+    <>
+      <BookCard />
+    </>
   );
 }
 
--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,24 +1,15 @@
-interface Book {
-  title: string;
-  author: string;
-  rating: number;
-  image: string;
-}
-
-function BookCard({ book }: { book: Book }) {
-  const { title, author, rating, image } = book;
+function BookCard() {
+  const fruits = ["mango", "Orange", "Lemon", "Apple"];
 
   return (
-    <div className="col-md-6 col-lg-3 mb-4">
-      <div className="card h-100">
-        <img src={image} className="card-img-top" alt={`${title} cover`} />
-        <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">Author: {author}</p>
-          <p className="card-text">Rating: {rating} / 5</p>
-        </div>
-      </div>
-    </div>
+    <>
+      <ul>
+        {fruits.map((fruit, index) => (
+          <li key={index}>{fruit}</li>
+        ))}
+      </ul>
+    </>
   );
 }
+
 export default BookCard;
